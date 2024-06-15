0fb0e30822438ff09f869a94b3c79a71f6a093bb
Guney Unay <guneymarketing17@hotmail.com>
2024-06-15 20:00:26 +0300

    :lipstick: ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
-  return <main className="flex items-center justify-center gap-10 px-4"></main>;
+  return (
+    <main className="flex items-center justify-center gap-10 px-4">
+      <Button>Deneme</Button>
+    </main>
+  );
 }
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ButtonHTMLAttributes } from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
@@ -7,7 +7,7 @@ const buttonVariants = cva("bg-white", {
     variant: {
       default: "bg-red-500",
       primary: "bg-blue-500",
-      secondary: "bg-green-500",
+      secondary: "bg-green-500 leading-10",
     },
     size: {
       default: "p-4",
@@ -17,8 +17,27 @@ const buttonVariants = cva("bg-white", {
   },
   defaultVariants: {
     variant: "default",
-    size: "default",
+    size: "large",
   },
 });
 
-export { buttonVariants };
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants>;
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  className,
+  variant,
+  size,
+  ...props
+}) => {
+  return (
+    <button
+      {...props}
+      className={cn(buttonVariants({ variant, size, className }))}
+    >
+      {children}
+    </button>
+  );
+};
+export { buttonVariants, Button };
