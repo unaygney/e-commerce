e990c5b1aaee060d78547a93a321bf0a4682f140
Guney Unay <guneymarketing17@hotmail.com>
2024-06-15 20:31:32 +0300

    :lipstick: added custom button variants

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,24 +2,31 @@ import React, { ButtonHTMLAttributes } from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-const buttonVariants = cva("bg-white", {
-  variants: {
-    variant: {
-      default: "bg-red-500",
-      primary: "bg-blue-500",
-      secondary: "bg-green-500 leading-10",
+const buttonVariants = cva(
+  "rounded font-medium transition-colors duration-300 ease-in-out",
+  {
+    variants: {
+      variant: {
+        primary: "bg-red-500",
+        secondary: "bg-blue-500",
+        tertiary: "bg-green-500 leading-10",
+        destructive: "",
+        linkGray: "",
+        linkColor: "",
+      },
+      size: {
+        medium: "inline-flex h-10  items-center  gap-1 px-[14px] text-[14px]",
+        large: "inline-flex h-11 items-center gap-1.5 px-4 text-base",
+        extraLarge: "inline-flex h-12 items-center gap-1.5 px-5 text-base",
+        large2XL: "inline-flex h-[60px] items-center gap-2.5 px-6 text-[18px]",
+      },
     },
-    size: {
-      default: "p-4",
-      small: "p-2",
-      large: "p-6",
+    defaultVariants: {
+      variant: "primary",
+      size: "medium",
     },
   },
-  defaultVariants: {
-    variant: "default",
-    size: "large",
-  },
-});
+);
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants>;
