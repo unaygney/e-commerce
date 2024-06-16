1b5962bdb2c441a06e04ba4bac6f859a0ae1bc85
Guney Unay <guneymarketing17@hotmail.com>
2024-06-16 12:33:58 +0300

    :lipstick: ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,24 @@
 "use client";
-import { Button, buttonVariants } from "@/components/button";
-import Link from "next/link";
+
+import { Badge } from "@/components/badge";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full items-center justify-center gap-10 px-4">
-      <div className="flex items-center gap-4">
-        <Button size="medium" disabled>
-          <p>deneme</p>
-        </Button>
-        <Button variant="secondary" size="large">
-          <p>deneme</p>
-        </Button>
-        <Button variant="tertiary" size="extraLarge">
-          <p>deneme</p>
-        </Button>
-        <Link
-          className={buttonVariants({ variant: "linkColor" })}
-          href="/about"
-        >
-          Go to about
-        </Link>
-        <Link className={buttonVariants({ variant: "linkGray" })} href="/about">
-          Go to about
-        </Link>
-
-        <Button variant="destructive" size="medium">
-          <p>deneme</p>
-        </Button>
+      <div>
+        <Badge variant="brand">Default</Badge>
+        <Badge variant="error" size="medium">
+          Default
+        </Badge>
+        <Badge variant="neutral" size="large">
+          Default
+        </Badge>
+        <Badge variant="success" size="medium">
+          Default
+        </Badge>
+        <Badge variant="warning" size="small">
+          Default
+        </Badge>
       </div>
     </main>
   );
