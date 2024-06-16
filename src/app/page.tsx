82caf096a315158ede1928e34dac4b62224c2b8f
Guney Unay <guneymarketing17@hotmail.com>
2024-06-16 03:44:35 +0300

    :lipstick: ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import { Button } from "@/components/button";
+"use client";
+import { Button, buttonVariants } from "@/components/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center gap-10 px-4">
-      <Button>Deneme</Button>
+    <main className="flex min-h-screen w-full items-center justify-center gap-10 px-4">
+      <div className="flex items-center gap-4">
+        <Button size="medium" disabled>
+          <p>deneme</p>
+        </Button>
+        <Button variant="secondary" size="large">
+          <p>deneme</p>
+        </Button>
+        <Button variant="tertiary" size="extraLarge">
+          <p>deneme</p>
+        </Button>
+        <Link
+          className={buttonVariants({ variant: "linkColor" })}
+          href="/about"
+        >
+          Go to about
+        </Link>
+        <Link className={buttonVariants({ variant: "linkGray" })} href="/about">
+          Go to about
+        </Link>
+
+        <Button variant="destructive" size="medium">
+          <p>deneme</p>
+        </Button>
+      </div>
     </main>
   );
 }
