a6f4cd88d634ced958772b1242d58c7251f2bb50
Guney Unay <guneymarketing17@hotmail.com>
2024-06-18 17:55:12 +0300

    :lipstick: input ui changes

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -15,7 +15,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           ref={ref}
           type={type}
           className={cn(
-            "w-full overflow-hidden text-ellipsis rounded border border-neutral-200 bg-neutral-50 py-2.5 pl-3.5 pr-8 text-sm font-normal leading-5 text-neutral-900 outline-none placeholder:text-neutral-500 focus:border-indigo-700 focus:ring-2 focus:ring-violet-200 focus:ring-offset-0 disabled:cursor-not-allowed disabled:border-neutral-100 disabled:text-[#a3a3a3]",
+            "w-full overflow-hidden text-ellipsis rounded border border-neutral-200 bg-neutral-50 py-2.5 pl-3.5 pr-8 text-sm font-normal leading-5 text-neutral-900 outline-none transition duration-200 ease-in-out placeholder:text-neutral-500 focus:border-indigo-700 focus:ring-2 focus:ring-violet-200 focus:ring-offset-0 disabled:cursor-not-allowed disabled:border-neutral-100 disabled:text-[#a3a3a3]",
             icon && "mr-2 pl-[42px]",
             className,
           )}
