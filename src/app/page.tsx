d2a1d71870671d33681036cb7ac54bb70bda27fd
Guney Unay <guneymarketing17@hotmail.com>
2024-06-19 01:40:57 +0300

    :lipstick: ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import { connectDB } from "@/lib/db";
 import React from "react";
-export default function Home() {
+import Product from "@/lib/models";
+import { IProduct } from "@/lib/definitions";
+export default async function Home() {
+  await connectDB();
+  const product = await Product.find({ name: "Classic Zip-Up Hoodie" }).lean();
+
+  const formattedProduct: IProduct[] = product.map(({ _id, name, ...rest }) => {
+    return {
+      id: _id?.toString(),
+      ...rest,
+    } as IProduct;
+  });
+
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
       <div className="flex w-[340px] flex-col gap-12"></div>
