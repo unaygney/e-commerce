268c13ffde84bf07f40fc765c28740176940d256
Guney Unay <guneymarketing17@hotmail.com>
2024-06-18 17:54:43 +0300

    :lipstick: ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,53 @@
 "use client";
 
-import { Badge } from "@/components/badge";
-import { Input } from "@/components/input";
 import { Label } from "@/components/label";
-import { CircleHelp } from "lucide-react";
+import { Textarea } from "@/components/textarea";
 
+import React from "react";
 export default function Home() {
+  const [message, setMessage] = React.useState("");
   return (
-    <main className="flex min-h-screen w-full items-center justify-center gap-10 px-4">
-      <div>
-        <Badge variant="brand">Default</Badge>
-        <Badge variant="error" size="medium">
-          Default
-        </Badge>
-        <Badge variant="neutral" size="large">
-          Default
-        </Badge>
-        <Badge variant="success" size="medium">
-          Default
-        </Badge>
-        <Badge variant="warning" size="small">
-          Default
-        </Badge>
-      </div>
-
-      <div className="w-full max-w-[300px]">
-        <div>
-          <Label htmlFor="name">Label</Label>
-          <Input type="text" id="name" placeholder="name@email.com" />
+    <main className="flex min-h-screen w-full items-center justify-center">
+      <div className="flex w-[340px] flex-col gap-8">
+        <div className="flex flex-col gap-1.5">
+          <Label htmlFor="name">Description</Label>
+          <Textarea
+            value={message}
+            placeholder="Write your message..."
+            className="h-[160px]"
+            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+              setMessage(e.target.value)
+            }
+          />
+          <p className="text-right text-sm font-normal leading-5 text-neutral-500">
+            {message.length}/500
+          </p>
+        </div>
+        <div className="flex flex-col gap-1.5">
+          <Label htmlFor="name">Description</Label>
+          <Textarea
+            value={message}
+            placeholder="Write your message..."
+            className="h-[103px]"
+            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+              setMessage(e.target.value)
+            }
+          />
+        </div>
+        <div className="flex flex-col gap-1.5">
+          <Label htmlFor="name">Description</Label>
+          <Textarea
+            value={message}
+            className="h-[160px]"
+            disabled
+            placeholder="Write your message..."
+            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+              setMessage(e.target.value)
+            }
+          />
+          <p className="text-right text-sm font-normal leading-5 text-neutral-500">
+            {message.length}/500
+          </p>
         </div>
       </div>
     </main>
