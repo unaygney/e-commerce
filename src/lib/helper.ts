f6ba47038771a601954737f310f00aec8b1f2bf4
Guney Unay <guneymarketing17@hotmail.com>
2024-06-22 01:51:00 +0300

    :hammer: created helper.ts

---
+++ src/lib/helper.ts
@@ -0,0 +1,7 @@
+export function adjustSize(size: string) {
+  if (size === "xs") return "XS";
+  if (size === "sm") return "S";
+  if (size === "md") return "M";
+  if (size === "lg") return "L";
+  if (size === "xl") return "XL";
+}
