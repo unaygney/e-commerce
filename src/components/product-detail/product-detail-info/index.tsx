2481f876ada83ec7f7f32e37170b426ad8196a5f
Guney Unay <guneymarketing17@hotmail.com>
2024-06-26 00:39:16 +0300

    :recycle: Refactor Product-detail component

--- src/components/product-detail/product-detail-info/index.tsx
+++ src/components/product-detail/product-detail-info/index.tsx
@@ -1,13 +1,16 @@
 "use client";
+//* React and Next
 import React from "react";
+import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+//* Components
 import { Rating } from "@mui/material";
 import { Badge } from "@/components/badge";
 import { Button } from "@/components/button";
 import { StarIcon, CheckIcon, Minus, Plus } from "lucide-react";
 import { RadioGroup } from "@headlessui/react";
-import { cn } from "@/lib/utils";
-import { useRouter } from "next/navigation";
-import { useSearchParams } from "next/navigation";
+import ProductReview from "@/components/product-review";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import {
   Accordion,
   AccordionContent,
@@ -20,8 +23,9 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+//* Utils
+import { cn } from "@/lib/utils";
 import { Product } from "@/lib/definitions";
-
 import { adjustSize } from "@/lib/helper";
 
 interface ProductDetailProps {
@@ -29,16 +33,77 @@ interface ProductDetailProps {
 }
 
 export function ProductDetailInfo({ product }: ProductDetailProps) {
-  const {
-    name,
-    description,
-    reviews = 0,
-    rating,
-    info,
-    inventory,
-    colors,
-    sizes,
-  } = product;
+  return (
+    <div className="flex w-full flex-1 flex-col lg:min-w-[48%]">
+      <ProductHeader product={product} />
+      <ProductController product={product} />
+      <ProductInfo product={product} />
+    </div>
+  );
+}
+
+function ProductHeader({ product }: ProductDetailProps) {
+  const { name, description, reviews = 0, rating, inventory } = product;
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-3xl font-semibold leading-9 text-neutral-900 md:text-5xl md:leading-[48px]">
+        {name}
+      </h1>
+      <div className="mt-5 flex flex-col">
+        <div className="flex items-end gap-2">
+          <span className="text-3xl font-medium leading-9 text-neutral-600">
+            ${inventory[0].sale_price}
+          </span>
+          <span className="text-lg font-medium leading-7 text-neutral-400 line-through">
+            ${inventory[0].list_price}
+          </span>
+        </div>
+        {inventory[0].discount && (
+          <Badge variant="warning" size="medium" className="mt-2 self-start">
+            %{inventory[0].discount_percentage} OFF
+          </Badge>
+        )}
+      </div>
+      <div className="mt-3 flex items-center gap-2">
+        <h6 className="text-xl font-normal leading-7 text-neutral-900">
+          {rating}
+        </h6>
+        <Rating
+          name="text-feedback"
+          readOnly
+          precision={0.5}
+          value={rating}
+          emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+        />
+        <Dialog>
+          <DialogTrigger>
+            <p className="text-sm font-medium leading-5 text-indigo-700">
+              {reviews === 0 && (
+                <>
+                  <span className="text-sm font-medium leading-5 text-neutral-500">
+                    No reviews yet.
+                  </span>
+                  Be the first.
+                </>
+              )}
+              {reviews === 1 && `See ${reviews} review`}
+              {reviews > 1 && `See all ${reviews} reviews`}
+            </p>
+          </DialogTrigger>
+          <DialogContent>
+            <ProductReview />
+          </DialogContent>
+        </Dialog>
+      </div>
+      <p className="my-8 text-base font-normal leading-6 text-neutral-600">
+        {description}
+      </p>
+    </div>
+  );
+}
+
+function ProductController({ product }: ProductDetailProps) {
+  const { inventory, colors, sizes } = product;
 
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -49,21 +114,13 @@ export function ProductDetailInfo({ product }: ProductDetailProps) {
   let sizeParam: string | number = searchParams.get("size") ?? defaultSize;
   const colorParam = searchParams.get("color") ?? defaultColor;
 
+  const [quantity, setQuantity] = React.useState<number>(1);
+  const [selected, setSelected] = React.useState<string>(colorParam);
+  const [curSize, setCurSize] = React.useState<string | number>(sizeParam);
+
   if (!isNaN(parseFloat(sizeParam as string))) {
     sizeParam = parseFloat(sizeParam as string);
   }
-
-  const [selected, setSelected] = React.useState<string>(colorParam);
-  const [curSize, setCurSize] = React.useState<string | number>(sizeParam);
-  const [quantity, setQuantity] = React.useState<number>(1);
-
-  const itemStock = inventory.find(
-    (item: any) =>
-      (item.size === sizeParam ||
-        (sizeParam === "std" && item.size === null) ||
-        (typeof sizeParam === "number" && item.size === sizeParam)) &&
-      item.color === colorParam,
-  );
 
   const filteredItemByColor: any = inventory.filter(
     (item: any) => item.color === colorParam,
@@ -91,58 +148,16 @@ export function ProductDetailInfo({ product }: ProductDetailProps) {
     }
   };
 
+  const itemStock = inventory.find(
+    (item: any) =>
+      (item.size === sizeParam ||
+        (sizeParam === "std" && item.size === null) ||
+        (typeof sizeParam === "number" && item.size === sizeParam)) &&
+      item.color === colorParam,
+  );
+
   return (
-    <div className="flex w-full flex-1 flex-col lg:min-w-[48%]">
-      <div className="flex flex-col">
-        <h1 className="text-3xl font-semibold leading-9 text-neutral-900 md:text-5xl md:leading-[48px]">
-          {name}
-        </h1>
-        <div className="mt-5 flex flex-col">
-          <div className="flex items-end gap-2">
-            <span className="text-3xl font-medium leading-9 text-neutral-600">
-              ${inventory[0].sale_price}
-            </span>
-            <span className="text-lg font-medium leading-7 text-neutral-400 line-through">
-              ${inventory[0].list_price}
-            </span>
-          </div>
-          {inventory[0].discount && (
-            <Badge variant="warning" size="medium" className="mt-2 self-start">
-              %{inventory[0].discount_percentage} OFF
-            </Badge>
-          )}
-        </div>
-        <div className="mt-3 flex items-center gap-2">
-          <h6 className="text-xl font-normal leading-7 text-neutral-900">
-            {rating}
-          </h6>
-          <Rating
-            name="text-feedback"
-            readOnly
-            precision={0.5}
-            value={rating}
-            emptyIcon={
-              <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
-            }
-          />
-          <p className="text-sm font-medium leading-5 text-indigo-700">
-            {reviews === 0 && (
-              <>
-                <span className="text-sm font-medium leading-5 text-neutral-500">
-                  No reviews yet.
-                </span>
-                Be the first.
-              </>
-            )}
-            {reviews === 1 && `See ${reviews} review`}
-            {reviews > 1 && `See all ${reviews} reviews`}
-          </p>
-        </div>
-      </div>
-      <p className="my-8 text-base font-normal leading-6 text-neutral-600">
-        {description}
-      </p>
-      {/* Product buttons */}
+    <>
       <div className="mb-8 flex flex-col">
         <div>
           <h6 className="mb-2 text-sm font-normal leading-5 text-neutral-500">
@@ -209,7 +224,6 @@ export function ProductDetailInfo({ product }: ProductDetailProps) {
           </RadioGroup>
         </div>
       </div>
-      {/* Price Button */}
       <div className="mb-8 flex flex-col gap-4">
         <h6 className="text-sm font-normal leading-5 text-neutral-500">
           Quantity
@@ -256,34 +270,39 @@ export function ProductDetailInfo({ product }: ProductDetailProps) {
       <Button disabled={itemStock?.stock === 0} variant="primary" size="large">
         Add to Cart
       </Button>
-      {/* Accordion Section */}
-      <Accordion type="multiple" className="mt-6 marker:text-neutral-600">
-        {info.map(
-          (
-            {
-              title,
-              description,
-            }: {
-              title: string;
-              description: String[];
-            },
-            index: number,
-          ) => (
-            <AccordionItem key={index} value={`item-${index + 1}`}>
-              <AccordionTrigger className="text-lg font-medium leading-7 text-neutral-900">
-                {title}
-              </AccordionTrigger>
-              <AccordionContent className="mt-2 text-base font-normal leading-6 text-neutral-600">
-                <ul className="list-inside list-disc">
-                  {description.map((desc, index) => (
-                    <li key={index}>{desc}</li>
-                  ))}
-                </ul>
-              </AccordionContent>
-            </AccordionItem>
-          ),
-        )}
-      </Accordion>
-    </div>
+    </>
+  );
+}
+
+function ProductInfo({ product }: ProductDetailProps) {
+  const { info } = product;
+  return (
+    <Accordion type="multiple" className="mt-6 marker:text-neutral-600">
+      {info.map(
+        (
+          {
+            title,
+            description,
+          }: {
+            title: string;
+            description: String[];
+          },
+          index: number,
+        ) => (
+          <AccordionItem key={index} value={`item-${index + 1}`}>
+            <AccordionTrigger className="text-lg font-medium leading-7 text-neutral-900">
+              {title}
+            </AccordionTrigger>
+            <AccordionContent className="mt-2 text-base font-normal leading-6 text-neutral-600">
+              <ul className="list-inside list-disc">
+                {description.map((desc, index) => (
+                  <li key={index}>{desc}</li>
+                ))}
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+        ),
+      )}
+    </Accordion>
   );
 }
