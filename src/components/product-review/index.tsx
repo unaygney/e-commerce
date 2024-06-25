29473604dc0d0052824d17264f4eeab39aebc1c0
Guney Unay <guneymarketing17@hotmail.com>
2024-06-26 00:38:08 +0300

    :lipstick: created ProductReview component

---
+++ src/components/product-review/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { DialogDescription, DialogTitle } from "@/components/ui/dialog";
+
+export default function ProductReview() {
+  return (
+    <>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>testing</DialogDescription>
+    </>
+  );
+}
