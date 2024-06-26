04ad0c5eebb108e59476be57be148f552402f1a9
Guney Unay <guneymarketing17@hotmail.com>
2024-06-26 20:48:54 +0300

    :hammer: fetching data

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -1,4 +1,11 @@
+"use server";
 import { Product } from "./definitions";
+
+interface ReviewQueryParams {
+  page?: number;
+  per_page?: number;
+  rating?: number;
+}
 
 export async function getProductById(id: string): Promise<Product | undefined> {
   try {
@@ -9,6 +16,37 @@ export async function getProductById(id: string): Promise<Product | undefined> {
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
+      },
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function getProductReview(id: string, params?: ReviewQueryParams) {
+  try {
+    let url = new URL(
+      `https://www.greatfrontend.com/api/projects/challenges/e-commerce/products/${id}/reviews`,
+    );
+
+    if (params) {
+      Object.keys(params).forEach((key) => {
+        if (params[key as keyof ReviewQueryParams] !== undefined) {
+          url.searchParams.append(
+            key,
+            params[key as keyof ReviewQueryParams]!.toString(),
+          );
+        }
+      });
+    }
+
+    const response = await fetch(url.toString(), {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
       },
     });
 
