a640186c31592b94c23a716580bef0d878e70fda
Guney Unay <guneymarketing17@hotmail.com>
2024-06-26 20:48:45 +0300

    :hammer: added react-quey

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import ReactQueryProvider from "@/components/Providers/ReactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} scroll-smooth antialiased`}>
-        {/* <Navbar /> */}
-        {children}
+        <ReactQueryProvider>
+          {/* <Navbar /> */}
+          {children}
+        </ReactQueryProvider>
       </body>
     </html>
   );
