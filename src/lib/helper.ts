6d8639b7eaf1ae491726f6b0a7218470b3888712
Guney Unay <guneymarketing17@hotmail.com>
2024-06-27 12:42:56 +0300

    :hammer: added helper func

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -11,3 +11,20 @@ export const toBase64 = (str: string) =>
   typeof window === "undefined"
     ? Buffer.from(str).toString("base64")
     : window.btoa(str);
+
+export function formatDate(dateString: string) {
+  if (!dateString) {
+    return "";
+  }
+
+  const date = new Date(dateString);
+  if (isNaN(date.getTime())) {
+    return "";
+  }
+
+  const day = date.getDate();
+  const monthName = date.toLocaleString("en-US", { month: "long" });
+  const year = date.getFullYear();
+
+  return `${monthName} ${day}, ${year}`;
+}
