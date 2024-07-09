b6bf2d5520f2adccfd966b6eded18f7f29e463d1
Guney Unay <guneymarketing17@hotmail.com>
2024-07-09 16:45:12 +0300

    :hammer: fetching real data

--- src/app/checkout/checkout-content/index.tsx
+++ src/app/checkout/checkout-content/index.tsx
@@ -1,11 +1,33 @@
+import { db } from "@/db";
 import CheckoutForm from "./checkout-form";
 import CheckoutSummary from "./checkout-summary";
+import { cookies } from "next/headers";
+import { notFound } from "next/navigation";
+export default async function CheckoutContent() {
+  const cookieStore = cookies();
+  const cartId = cookieStore.get("cart_id")?.value;
 
-export default function CheckoutContent() {
+  const cart = await db.cart.findUnique({
+    where: { cart_id: cartId },
+    include: {
+      items: {
+        include: {
+          product: true,
+          unit: true,
+        },
+      },
+      summary: true,
+    },
+  });
+
+  if (!cart) {
+    return notFound();
+  }
+
   return (
     <div className="flex w-full flex-col gap-8 xl:flex-row">
       <CheckoutForm />
-      <CheckoutSummary />
+      <CheckoutSummary basket={cart} />
     </div>
   );
 }
