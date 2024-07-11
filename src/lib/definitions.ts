692bb8f12de89603f394f05aef412ccbb27b0486
Guney Unay <guneymarketing17@hotmail.com>
2024-07-11 16:33:46 +0300

    :recycle: update definitions.ts

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,24 +1,3 @@
-import { Document, ObjectId } from "mongoose";
-
-export interface IProduct extends Document {
-  _id?: ObjectId | string;
-  id?: string | undefined;
-  name: string;
-  description: string;
-  price: number;
-  category: string;
-  stock: number;
-  images: string[];
-  sku: string;
-  createdAt: Date;
-  updatedAt: Date;
-  attributes: string;
-  discount?: number;
-  isPublished: boolean;
-  features: string[];
-  fabricCare: string;
-  shipping: string;
-}
 interface Category {
   category_id: string;
   name: string;
